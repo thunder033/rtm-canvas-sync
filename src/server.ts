edd05028736416ca75b7a53d93362ccd951e5a14
Greg <gjr8050@rit.edu>
2017-02-16 13:46:23 -0500

    start express app

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
+'use strict';
+/// <reference path="../node_modules/@types/node/index.d.ts" />
 /**
  * Created by gjrwcs on 2/16/2017.
  */
+
+import * as express from 'express';
+import * as http from 'http';
+
+const app: express.Application = express();
+
+app.get('/', (req: http.IncomingMessage, res: http.ServerResponse) => {
+    console.log('hello world');
+});
